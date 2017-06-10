58e2d98a62eebd2424d5304b182933f0050238a9
huffmabr <huffmabr@oregonstate.edu>
2017-06-10 15:52:35 -0700

    Filled in App Meat
    Filled in the main aprt of the code including the Adapter, the rycler
    view and the utilities.
    STILL NEED:
    -Correct JSON results from API call
    -FIX API call
    -Changes JSon splitter to GSON easy-peasy thing

---
+++ app/src/main/java/com/example/android/brewbee/SearchResultDetailActivity.java
@@ -0,0 +1,37 @@
+package com.example.android.brewbee;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.brewbee.R;
+import com.example.android.brewbee.utils.BreweryUtils;
+
+/**
+ * Created by Bradley on 6/10/2017.
+ */
+
+public class SearchResultDetailActivity extends AppCompatActivity {
+    private TextView mSearchResultNameTV;
+    private TextView mSearchResultDescriptionTV;
+    private TextView mSearchResultStarsTV;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_search_result_detail);
+
+        mSearchResultNameTV = (TextView)findViewById(R.id.tv_search_result_name);
+        mSearchResultDescriptionTV = (TextView)findViewById(R.id.tv_search_result_description);
+        mSearchResultStarsTV = (TextView)findViewById(R.id.tv_search_result_stars);
+
+        Intent intent = getIntent();
+        if(intent != null && intent.hasExtra(BreweryUtils.BrewItem.EXTRA_SEARCH_RESULT)){
+            BreweryUtils.BrewItem searchResult = (BreweryUtils.BrewItem)intent.getSerializableExtra(BreweryUtils.BrewItem.EXTRA_SEARCH_RESULT);
+            mSearchResultNameTV.setText(searchResult.fullname);
+            mSearchResultDescriptionTV.setText(searchResult.description);
+            mSearchResultStarsTV.setText(Integer.toString(searchResult.stars));
+        }
+    }
+}
